c1441e4f2b6f43f0b515baf08938598c057ee14a
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-02-27 21:49:05 +0330

    Add multiple sample animation and gestures

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import React from "react";
+import { Button, View, StyleSheet } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedProps,
+  useAnimatedStyle,
   withTiming,
-} from 'react-native-reanimated';
-import { Svg, Circle } from 'react-native-svg';
+} from "react-native-reanimated";
+import { Svg, Circle } from "react-native-svg";
+import "react-native-gesture-handler";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedCircle = Animated.createAnimatedComponent(Circle); // Making a custom animated component to handle its animation with props , the component is a circle that can get r as props
 
 export default function CounterScreen() {
   const r = useSharedValue<number>(20);
+  const pressed = useSharedValue<boolean>(false);
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      pressed.value = true;
+    })
+    .onFinalize(() => {
+      pressed.value = false;
+    });
 
   const handlePress = () => {
-    r.value += 10;
+    r.value += 10; // On button press increasing r by 10
   };
 
   // highlight-start
   const animatedProps = useAnimatedProps(() => ({
+    // Using animated props to transform r to its new value with timing
     r: withTiming(r.value),
   }));
   // highlight-end
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    backgroundColor: pressed.value ? "#FFE04B" : "#B58DF1", // Animated style changes background color when button pressed
+    transform: [{ scale: withTiming(pressed.value ? 1.2 : 1) }], // Animated styles transform (Scale) with timing
+  }));
 
   return (
     <View style={styles.container}>
@@ -30,10 +51,17 @@ export default function CounterScreen() {
           cy="50%"
           fill="#b58df1"
           // highlight-next-line
+          // Passing r with animated props that is taken by custom animated component and passed to the circle inside it
           animatedProps={animatedProps}
         />
       </Svg>
       <Button onPress={handlePress} title="Click me" />
+      <GestureHandlerRootView style={styles.container}>
+        <GestureDetector gesture={tap}>
+          {/*Its like handle click that makes a boolean true false when its taped*/}
+          <Animated.View style={[styles.circle, animatedStyles]} />
+        </GestureDetector>
+      </GestureHandlerRootView>
     </View>
   );
 }
@@ -41,10 +69,16 @@ export default function CounterScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   svg: {
     height: 250,
-    width: '100%',
+    width: "100%",
+  },
+  circle: {
+    height: 120,
+    width: 120,
+    borderRadius: 500,
+    marginTop: 46,
   },
 });
--- app/idea.tsx
+++ app/idea.tsx
@@ -1,9 +1,58 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+  withRepeat,
+} from "react-native-reanimated";
 
-export default function IdeaScreen() {
+const duration = 2000;
+
+interface AppProps {
+  width: number;
+}
+
+export default function IdeaScreen({ width = 50 }: AppProps) {
+  const defaultAnim = useSharedValue<number>(width / 2 - 160); // Default value that gets changed later inside useEffect
+  const linear = useSharedValue<number>(width / 2 - 160);
+
+  const animatedDefault = useAnimatedStyle(() => ({
+    transform: [{ translateX: defaultAnim.value }], // Animated styles that translates to the default value
+  }));
+  const animatedChanged = useAnimatedStyle(() => ({
+    transform: [{ translateX: linear.value }],
+  }));
+
+  React.useEffect(() => {
+    linear.value = withRepeat(
+      // highlight-next-line
+      withTiming(-linear.value, {
+        duration,
+        easing: Easing.linear,
+      }),
+      -1, // Change this to 1 and it will stop after one repeat, 2 and stop after 2 repeats, -1 = infinite
+      true, // Change this to false and it will repeat from start each time not forward backward
+    );
+    defaultAnim.value = withRepeat(
+      // highlight-next-line
+      withTiming(-defaultAnim.value, {
+        duration,
+      }),
+      -1,
+      true,
+    );
+  }, []);
+
   return (
-    <View style= {styles.container}>
-      <Text style = {styles.text}>Counter</Text>
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedDefault]}>
+        <Text style={styles.text}>inout</Text>
+      </Animated.View>
+      <Animated.View style={[styles.box, animatedChanged]}>
+        <Text style={styles.text}>linear</Text>
+      </Animated.View>
     </View>
   );
 }
@@ -11,11 +60,23 @@ export default function IdeaScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: "center",
+    height: "100%",
+  },
+  box: {
+    height: 80,
+    width: 80,
+    margin: 20,
+    borderWidth: 1,
+    borderColor: "#b58df1",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    fontSize: 24,
+    color: "#b58df1",
+    textTransform: "uppercase",
+    fontWeight: "bold",
   },
 });
